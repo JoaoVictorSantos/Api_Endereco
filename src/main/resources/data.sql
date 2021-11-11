da305352c52e67b1b34d6889f65a4cd365109063
JoaoVictorSantos <victordossantos.joao06@gmail.com>
2021-11-10 22:43:54 -0300

    Configuração inicial

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS ENDERECO(ID INT PRIMARY KEY, RUA VARCHAR(255), BAIRRO VARCHAR(255), CIDADE VARCHAR(255), ESTADO VARCHAR(255), CEP VARCHAR(255));
+
+
+INSERT INTO ENDERECO (id, rua, bairro, cidade, estado, cep) values (1, 'Avenida Paulista - de 1300/1301 a 1498/1499', 'Sede', 'São Paulo','São Paulo', '15370496');
+INSERT INTO ENDERECO (id, rua, bairro, cidade, estado, cep) values (2, 'Avenida Paulista - de 1700/1701 a 1798/1799', 'Vila São Francisco de Assis', 'São Paulo','São Paulo', '17560250');
